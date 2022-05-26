f8b902cb7c7e39aff099fb2ab25f3bf52d103821
Carlos Agarie <carlos.agarie@gmail.com>
2022-05-26 18:40:07 -0300

    Updated 10.hs

--- haskell/10.hs
+++ haskell/10.hs
@@ -2,4 +2,17 @@
 --
 -- Find the sum of all the primes below two million.
 
+-- Check 07.hs
 
+divisible :: (Integral a) => a -> a -> Bool
+divisible a b = a `rem` b == 0
+
+-- This is an incredibly dumb way to check for primeness lol
+isPrime :: Int -> Bool
+isPrime n = not . or $ map (divisible n) [2 .. (ceiling . sqrt . fromIntegral) n]
+
+sumOfPrimesBelow :: Int -> Int
+sumOfPrimesBelow n = 2 + (sum $ filter (\i -> isPrime i) [2..n])
+
+main :: IO ()
+main = putStrLn . show $ sumOfPrimesBelow 2000000
